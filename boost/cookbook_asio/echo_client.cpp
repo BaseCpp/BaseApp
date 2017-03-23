84ca8464e127b11ee80fca4edf0ec91ba7344def
chen_zhihui <chen_zhihui@cdv.com>
2017-03-23 20:23:16 +0800

    cookbook for boost::asio

---
+++ boost/cookbook_asio/echo_client.cpp
@@ -0,0 +1,83 @@
+﻿
+#pragma once
+
+#include <boost/asio/io_service.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/spawn.hpp>
+#include <boost/asio/steady_timer.hpp>
+#include <boost/asio/write.hpp>
+#include <iostream>
+#include <memory>
+
+class client : public std::enable_shared_from_this<client> {
+public:
+        client(boost::asio::io_service & io_service, int i):
+            socket(io_service),
+            timer(io_service),
+            strand(io_service),
+            id(i){
+            ;
+        }
+
+        void connect(const boost::asio::ip::tcp::endpoint & end) {
+            auto self = this->shared_from_this();
+            boost::asio::spawn(strand, [this, self, end](boost::asio::yield_context yield){
+//                boost::system::error_code ec;
+                socket.async_connect(end,  [this, self](boost::system::error_code ec){
+                    self->go();
+                });
+            });
+        }
+
+        void go() {
+            auto self = this->shared_from_this();
+            boost::asio::spawn(strand, [this, self](boost::asio::yield_context yield) {
+                try {
+                    char data[128];
+                    for(int i = 0; i < 100000000; i++){
+                        timer.expires_from_now(std::chrono::seconds(10));
+                        std::memcpy(data, &i, sizeof(i));
+                        socket.async_send(boost::asio::buffer(data),yield);
+                        socket.async_receive(boost::asio::buffer(data),yield);
+                        //std::cout<<"id="<<id<<", seq="<< i << std::endl;
+                    }
+                } catch ( std::exception * e ){
+                    socket.close();
+                    timer.cancel();
+                }
+            });
+            boost::asio::spawn(strand, [this, self](boost::asio::yield_context yield){
+                if( socket.is_open() ) {
+                    boost::system::error_code ec;
+                    timer.async_wait(yield[ec]);
+                    if( timer.expires_from_now() <= std::chrono::seconds(0))
+                        socket.close();
+                }
+            });
+        }
+
+private:
+    boost::asio::ip::tcp::socket socket;
+    boost::asio::steady_timer timer;
+    boost::asio::io_service::strand strand;
+    int id;
+};
+
+int main(int argc , char ** argv ) {
+    try {
+        long count = std::atoi(argv[1]);
+        std::cout << count <<std::endl;
+        boost::asio::io_service io_service;
+        boost::asio::spawn(io_service, [&io_service, argv, count](boost::asio::yield_context yield){
+            boost::asio::ip::tcp::resolver resolver(io_service);
+            auto end = resolver.resolve({boost::asio::ip::tcp::v4(), argv[2], argv[3]});
+            for(int i = 0; i < count; ++i ){
+                std::make_shared<client>(io_service, i)->connect(*end);
+            }
+        });
+        io_service.run();
+    } catch ( std::exception & e) {
+        std::cerr<<"Exception :" << e.what() << std::endl;
+        return 0;
+    }
+}
---
+++ boost/cookbook_asio/echo_server2.cpp
@@ -0,0 +1,90 @@
+﻿
+//
+// echo_server.cpp
+// ~~~~~~~~~~~~~~~
+//
+// Copyright (c) 2003-2017 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <boost/asio/io_service.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/spawn.hpp>
+#include <boost/asio/steady_timer.hpp>
+#include <boost/asio/write.hpp>
+#include <iostream>
+#include <memory>
+
+const int port = 8081;
+
+using namespace  boost::asio::ip;
+
+class session : public std::enable_shared_from_this<session> {
+public :
+    session(tcp::socket && sock)
+        :socket (std::move(sock)),
+         timer(socket.get_io_service()),
+         strand(socket.get_io_service()){
+         //nothing
+    }
+
+    void go() {
+        // keep to live untile end of go
+        // todo:: ~session will accur in worker thread, try to move to another thread
+        auto self = this->shared_from_this();
+        boost::asio::spawn(strand, [this, self](boost::asio::yield_context yield){
+            //keep to live within any clousure with this
+            try {
+                char data[128];
+                for(;;){
+                    timer.expires_from_now(std::chrono::seconds(10)); // reset timer
+                    auto n = socket.async_read_some(boost::asio::buffer(data), yield);
+                    //throw a exception when failed
+                    boost::asio::async_write(socket, boost::asio::buffer(data,n),yield);
+                }
+            } catch (std::exception & e){ //catch a exception when socket closed
+                socket.close();
+                timer.cancel();
+            }
+        });
+
+        boost::asio::spawn(strand, [this, self](boost::asio::yield_context yield){
+            while( socket.is_open()) {
+                boost::system::error_code ec;
+                timer.async_wait(yield[ec]);
+                if( timer.expires_from_now() <= std::chrono::seconds(0))
+                    socket.close();
+            }
+        });
+    }
+
+private:
+    tcp::socket socket;
+    boost::asio::steady_timer timer;
+    boost::asio::io_service::strand strand;
+};
+
+
+int main(int argc , char ** argv ) {
+    try {
+        boost::asio::io_service io_service;
+        boost::asio::spawn(io_service, [&io_service](boost::asio::yield_context yield ){
+            boost::asio::ip::tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), port) );
+            for(;;) {
+                boost::system::error_code ec;
+                boost::asio::ip::tcp::socket socket(io_service);
+                acceptor.async_accept(socket, yield[ec]);
+                if(!ec) {
+//                    std::cout<< socket.get_option()
+                    std::make_shared<session>(std::move(socket))->go();
+                }
+            }
+        });
+        io_service.run();
+    } catch ( std::exception & e) {
+        std::cerr<<"Exception :" << e.what() << std::endl;
+        return 0;
+    }
+}
